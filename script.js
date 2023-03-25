5aac1849ba104730b386602ec145a7a0240f600a
Durgesh_Sonkusare <7276104188d@gmail.com>
2023-03-25 09:28:56 +0530

    eighth commit

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ close.addEventListener("click", () => {
     })
 
 
+// sproduct js 
+
+const mainimg = document.getElementById("main-img");
+const smallimg = document.getElementsByClassName("small-img");
+
+
+    smallimg[0].addEventListener("click", () => 
+    mainimg.src=smallimg[0].src
+   )
+
+   smallimg[1].addEventListener("click", () => 
+   mainimg.src=smallimg[1].src
+  )
+
+  smallimg[2].addEventListener("click", () => 
+  mainimg.src=smallimg[2].src
+ )
+
+ smallimg[3].addEventListener("click", () => 
+ mainimg.src=smallimg[3].src
+)
+
